aad8b035199c8035e1cc21fddf31087ac7facc02
educamargob <ecamargoborges@gmail.com>
2022-04-25 21:09:58 -0300

    Adicionando aula 25/04

---
+++ aula0425/database/migrations/2022_04_11_223743_create_enderecos.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->string('logradouro', 100);
+            $table->integer('numero')->nullable();
+            $table->integer('cep');
+            $table->string('bairro', 30);
+            $table->string('cidade', 50);
+            $table->string('estado', 2);
+            $table->unsignedBigInteger('id_cliente');
+
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+};
